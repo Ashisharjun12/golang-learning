12ab06d197d9dbdcf108b2d71d0f044d26d480d2
Ashish Raj <ashishrahul748@gmail.com>
2025-03-10 13:51:09 +0530

    memeory

---
+++ 06memory/main.go
@@ -0,0 +1,19 @@
+package main
+
+
+func main(){
+
+
+}
+
+// in go memeory mamnage  haev declere  
+
+//1 . new()  and  make()
+
+
+//read these 
+
+// https://go.dev/ref/mem
+
+
+//https://www.calsoftinc.com/blogs/golang-memory-management.html
