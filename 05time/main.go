34752e3d69e28eec159748fb142acbe1696a0c34
Ashish Raj <ashishrahul748@gmail.com>
2025-03-10 13:24:34 +0530

    time

--- 05time/main.go
+++ 05time/main.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
-	
+func main() {
+	fmt.Println("Welcome to time in Go")
 
+	presentTime := time.Now()
 
+	fmt.Println("Current time is: ", presentTime)
+	fmt.Println("currnet time is :" ,presentTime.format("01-02-2006 15:04:05"))
 
 }
