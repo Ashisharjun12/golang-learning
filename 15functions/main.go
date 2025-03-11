1ca7607db7e83d60ce1b7981d33a36ccdab584d6
Ashish Raj <ashishrahul748@gmail.com>
2025-03-12 00:00:55 +0530

    functions

--- 15functions/main.go
+++ 15functions/main.go
@@ -1,8 +1,45 @@
 package main
 
+import "fmt"
+
+func add(a ,b int) int {
+	return a + b
+
+}
+
+func getlang() (string, string, string){
+	return "a", "b" , "c"
+}
+
+func main() {
+	res := add(1, 2)
+
+	fmt.Println("adding numbers:", res)
+
+	lang1 , lang2 , _ := getlang()
+
+	fmt.Println("dta lang", lang1 , lang2 )
 
 
-func main(){
+	// fmt.Println("sum", sums(1,2,3,4,5))
 
-	
+	numbers := []int{1,2,3,4,5}
+
+	result := sums(numbers...)
+
+	fmt.Println("sums num", result)
+}
+
+
+//varadic functions
+
+func sums(nums ...int) int{
+	total := 0
+
+	for _ , num := range nums{
+		total =total + num
+	}
+
+	return total
+
 }
